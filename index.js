5c50408d9c5ba0e8574c2a8e79ce9bbe2074c145
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-10-27 13:10:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Create an array named groupedNumbers
+const groupedNumbers = [];
+
+// Add an array containing 1, 3, and 5
+groupedNumbers.push([1, 3, 5]);
+
+// Add an array containing 2, 4, and 6
+groupedNumbers.push([2, 4, 6]);
+
+// Assign the innerHTML of the element with id "result" to groupedNumbers
+document.getElementById('result').innerHTML = groupedNumbers;
 
